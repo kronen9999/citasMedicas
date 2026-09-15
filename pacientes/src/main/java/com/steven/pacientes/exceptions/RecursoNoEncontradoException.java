dbcfa7a0126dc2e8caadc05db5a7b61b2bb4943a
kronen9999 <stacica22eneronkio@gmail.com>
2026-09-14 18:06:22 -0600

    Se ha eliminado la carpeta de pacientes y se ajustara por una nueva desde 0

--- pacientes/src/main/java/com/steven/pacientes/exceptions/RecursoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.steven.pacientes.exceptions;
-
-public class RecursoNoEncontradoException extends RuntimeException {
-    public RecursoNoEncontradoException(String message) {
-        super(message);
-    }
-}
--- pacientes/src/main/java/com/steven/pacientes/mappers/CommonMapper.java
+++
@@ -1,9 +0,0 @@
-package com.steven.pacientes.mappers;
-
-public interface CommonMapper <RQ,RS,E>{
-
-    E responseAEntidad(RQ request);
-
-    RS entidadAResponse(E entidad);
-
-}
